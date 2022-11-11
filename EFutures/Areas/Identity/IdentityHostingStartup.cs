551df1b0fb0c1971ca67c7b4475fdd5226b177c9
MarkJ-JLM <ranishkajayathilaka@gmail.com>
2022-11-11 16:28:36 +0530

    Main code

---
+++ EFutures/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using EFutures.Areas.Identity.Data;
+using EFutures.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(EFutures.Areas.Identity.IdentityHostingStartup))]
+namespace EFutures.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<EFutureDbContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("EFutureDbContextConnection")));
+
+                services.AddDefaultIdentity<ApplicationUser>(options => {
+                    options.SignIn.RequireConfirmedAccount = false;
+                    options.Password.RequireLowercase = false;
+                    options.Password.RequireUppercase = false;
+                    
+                    })
+                    .AddEntityFrameworkStores<EFutureDbContext>();
+            });
+        }
+    }
+}
---
+++ EFutures/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EFutures.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
